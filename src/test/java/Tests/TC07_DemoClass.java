492524fc8a99b22f29864ef411f94822a38ef001
Allosh <alitstr001@gmail.com>
2025-05-20 19:26:30 +0300

    Add isOlder method in TC07_DemoClass.java

--- src/test/java/Tests/TC07_DemoClass.java
+++ src/test/java/Tests/TC07_DemoClass.java
@@ -6,4 +6,9 @@ public class TC07_DemoClass {
     public TC07_DemoClass() {
         System.out.println("Hi, i am constractor");
     }
+
+    //add isOlder method
+    public boolean isOlder(double age) {
+        return age > 60.0;
+    }
 }
